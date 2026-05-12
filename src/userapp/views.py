eb424cdda31da742353f648f715b0313123583e0
Rahul vishwakarma <rahul1142000@gmail.com>
2026-05-12 11:06:27 +0400

    login and sinp page

---
+++ src/userapp/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
+# Create your views here.
+
+def registerView(request):
+
+    if(request.method == "POST"):
+          fname=request.POST.get("firstname")
+          lname=request.POST.get("lastname")
+          email=request.POST.get("email")
+          password=request.POST.get("password")
+          user=User.objects.create_user(username=email,first_name=fname,last_name=lname,password=password)
+          user.save()
+          return redirect("/accounts/login")
+
+    else:
+        return render(request,"register.html");
+ 
+
+@login_required
+def homeView(request):
+     return render(request,"home.html" ,{"user":request.user});
+
+
+
+def loginView(request):
+     if(request.method == "POST"):
+          email=request.POST.get("email")
+          password=request.POST.get("password")
+          user=authenticate(username=email,password=password)
+          if(user is not None):
+              login(request,user)
+              return redirect("/home")
+               
+          else:
+            return redirect("/accounts/login")
+     
+     else:
+     
+      return render(request,"login.html")
+     
+
+
+
+def logoutView(request):
+    logout(request)
+    return redirect("/accounts/login")
